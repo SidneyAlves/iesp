7fd181971019669228cf7632a293834bb5342f60
Sidney Alves <sidney.27.alves@gmail.com>
2018-01-17 10:43:25 -0200

    Site Kelly

---
+++ header.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <title>IESP</title>
+  </head>
+  <body>
+    <header class="container-fluid">
+    <a name="topo"></a>
+      <!-- Logo -->
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 logo">
+          <img src="img/logo.jpg" width=100% height=100% alt="">  
+          </div>
+          <div class="col-md-3"></div>
+          <div class="col-md-3 link">
+              <h2 class="text-center">Redes Sociais:</h2>
+              Nos siga no Facebook:<br>
+              <a href="https://www.facebook.com/iespmogi/" target=“_blank”><img src="img/fb-icon.png" width="64px" height="64px" align="right" alt="Icone Facebook"></a>
+              <br> <br> <br>   Fale conosco no Whatsapp:<br> +55 11 97284-1392   <img src="img/wpp-icon.png" width="64px" align="right" height="64px" alt=""> 
+
+          </div>
+
+        </div>
+      </div>
+
+        <div class="container-fluid">
+            <div class="row">  
+                <div class="col-md-12 menu">
+                    <ul class="nav">
+                        <li class="nav-item">
+                        <a class="nav-link active" href="#topo">Home</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#textos">Lorem Ipsum</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#sobrenos">Sobre nós</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#form">Contato</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#mapa">Mapa</a>
+                        </li>
+                    </ul>
+                </div> 
+            </div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,126 @@
+<?php include("header.php"); ?>
+    <section class="container slide">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="img/agua-viva.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="img/agua-viva.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="img/agua-viva.jpg" alt="Third slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+    </section>
+
+    <section class="container-fluid plx1">
+    </section>
+
+    <section class="container-fluid">
+    <a name="textos"></a>
+        <div class="container textos">
+            <center><h1>Lorem Ipsum</h1></center>
+            <div class="row">
+                <div class="col-md-4">
+                    <h2 class="text-center">Lorem Ipsum</h2>
+                    <img src="img/agua-viva.jpg" width=100% alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu aliquam sapien. Aenean congue euismod dolor nec scelerisque. 
+                        Nulla efficitur orci eget nisi ultrices congue. Praesent pellentesque tempor convallis. Sed enim odio, 
+                        vestibulum a posuere sit amet, vulputate eget ligula. Vivamus imperdiet vitae nibh ut euismod.</p>
+                </div>
+
+                <div class="col-md-4">
+                    <h2 class="text-center">Lorem Ipsum</h2>
+                    <img src="img/agua-viva.jpg" width=100% alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu aliquam sapien. Aenean congue euismod dolor nec scelerisque. 
+                        Nulla efficitur orci eget nisi ultrices congue. Praesent pellentesque tempor convallis. Sed enim odio, 
+                        vestibulum a posuere sit amet, vulputate eget ligula. Vivamus imperdiet vitae nibh ut euismod.</p>
+                </div>
+
+                <div class="col-md-4">
+                    <h2 class="text-center">Lorem Ipsum</h2>
+                    <img src="img/agua-viva.jpg" width=100% alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu aliquam sapien. Aenean congue euismod dolor nec scelerisque. 
+                        Nulla efficitur orci eget nisi ultrices congue. Praesent pellentesque tempor convallis. Sed enim odio, 
+                        vestibulum a posuere sit amet, vulputate eget ligula. Vivamus imperdiet vitae nibh ut euismod.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="container-fluid">
+        <a name="sobrenos"></a>
+        <div class="container sobrenos">
+            <h1 class="text-center">Sobre nós</h1>
+            <img src="img/agua-viva.jpg" width="100%" alt="">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu aliquam sapien. Aenean congue euismod dolor nec scelerisque. 
+                Nulla efficitur orci eget nisi ultrices congue. Praesent pellentesque tempor convallis. Sed enim odio, 
+                vestibulum a posuere sit amet, vulputate eget ligula. Vivamus imperdiet vitae nibh ut euismod.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu aliquam sapien. Aenean congue euismod dolor nec scelerisque. 
+                Nulla efficitur orci eget nisi ultrices congue. Praesent pellentesque tempor convallis. Sed enim odio, 
+                vestibulum a posuere sit amet, vulputate eget ligula. Vivamus imperdiet vitae nibh ut euismod.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu aliquam sapien. Aenean congue euismod dolor nec scelerisque. 
+                Nulla efficitur orci eget nisi ultrices congue. Praesent pellentesque tempor convallis. Sed enim odio, 
+                vestibulum a posuere sit amet, vulputate eget ligula. Vivamus imperdiet vitae nibh ut euismod.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu aliquam sapien. Aenean congue euismod dolor nec scelerisque. 
+                Nulla efficitur orci eget nisi ultrices congue. Praesent pellentesque tempor convallis. Sed enim odio, 
+                vestibulum a posuere sit amet, vulputate eget ligula. Vivamus imperdiet vitae nibh ut euismod.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu aliquam sapien. Aenean congue euismod dolor nec scelerisque. 
+                Nulla efficitur orci eget nisi ultrices congue. Praesent pellentesque tempor convallis. Sed enim odio, 
+                vestibulum a posuere sit amet, vulputate eget ligula. Vivamus imperdiet vitae nibh ut euismod.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu aliquam sapien. Aenean congue euismod dolor nec scelerisque. 
+                Nulla efficitur orci eget nisi ultrices congue. Praesent pellentesque tempor convallis. Sed enim odio, 
+                vestibulum a posuere sit amet, vulputate eget ligula. Vivamus imperdiet vitae nibh ut euismod.
+            </p>
+        </div>
+    </section>
+
+    <section class="container-fluid plx2">
+    </section>
+
+    <section class="container-fluid">
+    <a name="form"></a>
+        <div class="container form">
+            <h1 class="text-center">Entre em contato</h1>
+
+            <form action="enviar.php" id="contato" method="post">
+                <div class="form-group">
+                    <input type="text" class="form-control" required="true" placeholder="Nome">
+                </div>
+                <div class="form-group">
+                    <input type="email" class="form-control" required="true" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <textarea class="form-control">Mensagem</textarea>
+                </div>
+
+                <center><button type="submit" class="btn btn-primary">Enviar</button></center>
+            </form>
+
+        </div>
+    </section>
+
+    <section class="container-fluid">
+    <a name="mapa"></a>
+        <div class="container mapa">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d457.2786048754755!2d-46.18049519102309!3d-23.524270411190013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cdd84874c751f3%3A0x63e51a29992513a1!2sR.+Cruzeiro+do+Sul%2C+267+-+Vila+Oliveira%2C+Mogi+das+Cruzes+-+SP%2C+08790-170!5e0!3m2!1spt-BR!2sbr!4v1516184601432"
+            width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </section>
+<?php include("footer.php"); ?>
